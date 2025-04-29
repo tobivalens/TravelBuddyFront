85eae3788554723a65ce6cb86098a32602e7df91
Andres Bueno <andres.bueno1909@gmail.com>
2025-04-28 23:08:19 -0500

    Updated Database and readme

---
+++ databaseConfig/fromwindows.bat
@@ -0,0 +1,11 @@
+@echo off
+
+SET CONTAINER_NAME=directusmoviles-db-1
+SET SQL_FILE=.\data.sql
+SET DB_NAME=directus
+SET DB_USER=directus
+
+docker cp "%SQL_FILE%" "%CONTAINER_NAME%:/tmp/data.sql"
+docker exec -i %CONTAINER_NAME% psql -U %DB_USER% -d %DB_NAME% -f /tmp/data.sql -w
+
+pause
