a3e916a3c08bdb867533ad39560e1f33d76f8c42
tobivalens <nannokvalens@gmail.com>
2025-05-18 21:30:27 -0500

    pantallas ver evento y ver evento admin

--- app/src/main/java/com/example/travelbuddyapp/MainActivity.kt
+++ app/src/main/java/com/example/travelbuddyapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -39,6 +40,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.InsertDriveFile
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForwardIos
@@ -54,6 +56,7 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Settings
@@ -77,9 +80,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import com.example.travelbuddyapp.viewmodel.AUTH_STATE
 import androidx.compose.ui.platform.LocalContext
+import com.example.travelbuddyapp.resources.ui.screens.AppBottomNavigationBar
+import com.example.travelbuddyapp.resources.ui.screens.CustomTabBar
 import com.example.travelbuddyapp.resources.ui.screens.HomeScreen
 import com.example.travelbuddyapp.resources.ui.screens.TravelItem
 import java.text.SimpleDateFormat
@@ -162,6 +169,9 @@ fun AppNavigator() {
         composable("editEvent") { EditEventScreen(navController) }
     }
 }
+
+
+
 
 @Composable
 fun RegisterUserScreen() {
@@ -1460,3 +1470,448 @@ fun DatePickerModal(
         DatePicker(state = datePickerState)
     }
 }
+
+
+
+// ---------------------- VISUALIZE SCREEN NORMAL-----------------------------
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VisualizeEventScreen(
+    eventTitle: String = "Viaje a la montaña",
+    onBackClick: () -> Unit = {}
+) {
+    val scrollState = rememberScrollState()
+    var selectedTab by remember { mutableStateOf(0) }
+    val tabs = listOf("Evento", "Gastos", "Actividades")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = eventTitle,
+                        color = Color.White,
+                        fontSize = 28.sp,
+                        fontFamily = SaralaFont,
+                        maxLines = 1
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color(0xFFA181FA)
+                )
+            )
+        }
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(scrollState)
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+            Spacer(Modifier.height(12.dp))
+
+            // Tabs
+            CustomTabBar(
+                tabs = tabs,
+                selectedTab = selectedTab,
+                onTabSelected = { selectedTab = it }
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo), // reemplaza con tu imagen
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            // la descripcion
+            Text(
+                text = "Disfruta de una experiencia única entre paisajes imponentes, aire puro y tranquilidad absoluta. Este viaje a las montañas te lleva a explorar senderos escondidos, miradores panorámicos y cascadas cristalinas...",
+                color = Color(0xFFCBC7C7),
+                fontSize = 12.sp,
+                fontFamily = SaralaFont
+            )
+
+            Spacer(Modifier.height(24.dp))
+
+            // Fechas estaticas por ahora
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Text("Fecha de inicio", color = Color(0xFFA38AFB), fontWeight = FontWeight.Bold)
+                    Text("Julio 04, 2025", color = Color.Gray)
+                }
+                Column {
+                    Text("Fecha de finalización", color = Color(0xFFA38AFB), fontWeight = FontWeight.Bold)
+                    Text("Julio 30, 2025", color = Color.Gray)
+                }
+            }
+
+            Spacer(Modifier.height(24.dp))
+
+            // lista users
+            Text("Participantes", color = Color(0xFFA38AFB), fontWeight = FontWeight.Bold)
+
+            Spacer(Modifier.height(8.dp))
+
+            val participantes = listOf("Jairo Vélez", "Sofía Puente", "Jordi Arroyo")
+            participantes.forEach {
+                ParticipantItem(name = it)
+            }
+
+            Spacer(Modifier.height(8.dp))
+
+            //
+            OutlinedButton(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth(),
+                border = BorderStroke(1.dp, Color(0xFFA38AFB))
+            ) {
+                Text("+ Código de Evento", color = Color(0xFFA38AFB))
+            }
+
+            Spacer(Modifier.height(24.dp))
+            //ARCHIVOS
+            Text(
+                text = "Archivos",
+                color = Color(0xFF8C69FF),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Comparte aquí fotos, documentos o cualquier archivo importante del viaje. Todo lo que subas estará disponible para que todos los participantes lo vean y accedan fácilmente.",
+                color = Color.Gray,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .border(1.dp, Color.LightGray, RoundedCornerShape(10.dp))
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.InsertDriveFile,
+                        contentDescription = "Archivo",
+                        tint = Color.LightGray
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "No hay archivos aún",
+                        color = Color.LightGray,
+                        fontSize = 14.sp
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF8C69FF)),
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier.align(Alignment.Start)
+            ) {
+                Text(text = "Subir", color = Color.White)
+            }
+        }
+    }
+}
+
+
+//------------------------VISUALIZE SCREEN ADMIN-----------------------------
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VisualizeEventScreenAdmin(
+    eventTitle: String = "Viaje a la montaña",
+    onBackClick: () -> Unit = {}
+) {
+    val scrollState = rememberScrollState()
+    var selectedTab by remember { mutableStateOf(0) }
+    val tabs = listOf("Evento", "Gastos", "Actividades")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = eventTitle,
+                        color = Color.White,
+                        fontSize = 28.sp,
+                        fontFamily = SaralaFont,
+                        maxLines = 1
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
+                    }
+                },  actions = {
+                    IconButton(onClick = { /* acción editar */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = "Editar",
+                            tint = Color.White
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color(0xFFA181FA)
+                )
+            )
+        }
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(scrollState)
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+            Spacer(Modifier.height(12.dp))
+
+            // Tabs
+            CustomTabBar (
+                tabs = tabs,
+                selectedTab = selectedTab,
+                onTabSelected = { selectedTab = it }
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo), // reemplaza con tu imagen
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = "Disfruta de una experiencia única entre paisajes imponentes, aire puro y tranquilidad absoluta. Este viaje a las montañas te lleva a explorar senderos escondidos, miradores panorámicos y cascadas cristalinas...",
+                color = Color(0xFFCBC7C7),
+                fontSize = 12.sp,
+                fontFamily = SaralaFont
+            )
+
+            Spacer(Modifier.height(24.dp))
+
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Text("Fecha de inicio", color = Color(0xFFA38AFB), fontWeight = FontWeight.Bold)
+                    Text("Julio 04, 2025", color = Color.Gray)
+                }
+                Column {
+                    Text("Fecha de finalización", color = Color(0xFFA38AFB), fontWeight = FontWeight.Bold)
+                    Text("Julio 30, 2025", color = Color.Gray)
+                }
+            }
+
+            Spacer(Modifier.height(24.dp))
+
+            Text("Participantes", color = Color(0xFFA38AFB), fontWeight = FontWeight.Bold)
+
+            Spacer(Modifier.height(8.dp))
+
+            val participantes = listOf("Jairo Vélez", "Sofía Puente", "Jordi Arroyo")
+            participantes.forEach {
+                ParticipantItem(name = it)
+            }
+
+            Spacer(Modifier.height(8.dp))
+
+            // Botón para añadir participante
+            OutlinedButton(
+                onClick = { /* Acción */ },
+                modifier = Modifier.fillMaxWidth(),
+                border = BorderStroke(1.dp, Color(0xFFA38AFB))
+            ) {
+                Text("+ Añadir participante", color = Color(0xFFA38AFB))
+            }
+
+            Spacer(Modifier.height(24.dp))
+            //ARCHIVOS
+            Text(
+                text = "Archivos",
+                color = Color(0xFF8C69FF),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Comparte aquí fotos, documentos o cualquier archivo importante del viaje. Todo lo que subas estará disponible para que todos los participantes lo vean y accedan fácilmente.",
+                color = Color.Gray,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .border(1.dp, Color.LightGray, RoundedCornerShape(10.dp))
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.InsertDriveFile,
+                        contentDescription = "Archivo",
+                        tint = Color.LightGray
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "No hay archivos aún",
+                        color = Color.LightGray,
+                        fontSize = 14.sp
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                onClick = { /* Subir archivo */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF8C69FF)),
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier.align(Alignment.Start)
+            ) {
+                Text(text = "Subir", color = Color.White)
+            }
+        }
+    }
+}
+
+//-------participantes---------------------------------------
+
+
+@Composable
+fun ParticipantItem(name: String) {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = null,
+                tint = Color(0xFF8C69FF),
+                modifier = Modifier.size(32.dp)
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(
+                text = name,
+                color = Color.Gray,
+                fontSize = 16.sp,
+                modifier = Modifier.weight(1f)
+            )
+            IconButton(onClick = { /* Opciones */ }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Opciones",
+                    tint = Color.Gray
+                )
+            }
+        }
+
+        HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+    }
+}
+
+
+
+@Composable
+fun HomeEventScreen() {
+
+    val bottomController= rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            AppBottomNavigationBar(bottomController, selectedItem = 0, onOptionClick = {})
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = bottomController,
+            startDestination = "visualizeEvent",
+            modifier = Modifier.padding(innerPadding) 
+        ) {
+            composable("visualizeEvent") {
+                VisualizeEventScreen()
+            }
+            //esto era para pruebas
+            composable("recoverPassword") {
+                VisualizeEventScreenAdmin()
+            }
+
+            composable("1") {
+                // arreglar las rutas cuando esten completas
+
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
